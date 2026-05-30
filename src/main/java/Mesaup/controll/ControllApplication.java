3ca287c43dc6d3d9ea1fc1ff387033010576c52d
mateus <mateusalcantara124@gmail.com>
2026-05-30 18:24:53 -0300

    "commit main"

---
+++ src/main/java/Mesaup/controll/ControllApplication.java
@@ -0,0 +1,13 @@
+package Mesaup.controll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mesaup/controll/ControllApplicationTests.java
@@ -0,0 +1,13 @@
+package Mesaup.controll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
